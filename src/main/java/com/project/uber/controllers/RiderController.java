e432c854f0de4b035e6974184e1b189864c60da5
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2025-02-11 09:35:47 +0530

    api added for riders

--- src/main/java/com/project/uber/controllers/RiderController.java
+++ src/main/java/com/project/uber/controllers/RiderController.java
@@ -1,13 +1,12 @@
 package com.project.uber.controllers;
 
-import com.project.uber.dto.RideRequestDto;
+import com.project.uber.dto.*;
 import com.project.uber.services.RiderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rider")
@@ -18,5 +17,27 @@ public class RiderController {
     @PostMapping("/requestRide")
     public ResponseEntity<RideRequestDto> requestRide(@RequestBody RideRequestDto rideRequestDto){
         return ResponseEntity.ok(riderService.requestRide(rideRequestDto));
+    }
+
+    @PostMapping("/cancelRide/{rideId}")
+    public ResponseEntity<RideDto> cancelRide(@PathVariable Long rideId){
+        return ResponseEntity.ok(riderService.cancelRide(rideId));
+    }
+
+    @PostMapping("/rateDriver")
+    public ResponseEntity<DriverDto> rateDriver(@RequestBody RatingDto ratingDto){
+        return ResponseEntity.ok(riderService.rateDriver(ratingDto.getRideId(), ratingDto.getRating()));
+    }
+
+    @GetMapping("/getMyProfile")
+    public ResponseEntity<RiderDto> getMyProfile(){
+        return ResponseEntity.ok(riderService.getMyProfile());
+    }
+
+    @GetMapping("/getAllMyRides")
+    public ResponseEntity<Page<RideDto>> getAllMyRides(@RequestParam(defaultValue = "0") Integer pageOffset,
+                                                                                          @RequestParam(defaultValue = "10") Integer pageSize){
+        PageRequest pageRequest = PageRequest.of(pageOffset, pageSize);
+        return ResponseEntity.ok(riderService.getAllMyRides(pageRequest));
     }
 }
---
+++ src/main/java/com/project/uber/dto/RatingDto.java
@@ -0,0 +1,9 @@
+package com.project.uber.dto;
+
+import lombok.Data;
+
+@Data
+public class RatingDto {
+    private Long rideId;
+    private Integer rating;
+}
